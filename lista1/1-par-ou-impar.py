ad4388a5647f2090e282180f7bd5dc04fb3c24fa
Fernando Filho <fernando@dimap.ufrn.br>
2022-05-09 11:15:10 -0300

    dia 9 maio 2022

--- lista1/1-par-ou-impar.py
+++ lista1/1-par-ou-impar.py
@@ -2,12 +2,12 @@
 Escreva um programa que lê um número inteiro, 
 e imprime se esse número é par ou ímpar.
 ''' 
+
+# Recebe um número inteiro e retorna verdadeiro caso ele seja par e falso
+# quando ele for ímpar.
 def par(i):
-    resto = i % 2
-    if resto == 0:
-        return True
-    else:
-        return False
+    r = i % 2
+    return r == 0
 
 def impar(i):
     return not par(i)
@@ -16,8 +16,7 @@ mensagem = "Digite um número inteiro: "
 entrada = input(mensagem)
 i = int(entrada)
 
-if par(i):
-    print("O número é par.")
-
 if impar(i):
     print("O número é ímpar.")
+else:
+    print("O número é par.")
--- lista1/2-par-ou-impar.py
+++ lista1/2-par-ou-impar.py
@@ -1,6 +1,24 @@
 '''
 Escreva um programa que lê dois números inteiros, 
 e imprime uma mensagem caso ambos sejam pares ou
-caso ambos sejam ímpares.
+caso ambos sejam ímpares. Ele não fala nada quando
+um é par e o outro é ímpar.
 ''' 
+# Recebe um número inteiro e retorna verdadeiro caso ele seja par e falso
+# quando ele for ímpar.
+def par(i):
+    r = i % 2
+    return r == 0
+
+def impar(i):
+    return not par(i)
+
+x = int(input("Digite o primeiro número inteiro: "))
+y = int(input("Digite o segundo número inteiro: "))
+
+if par(x) and par(y):
+    print("Ambos são pares.")
+
+if impar(x) and impar(y):
+    print("Ambos são ímpares.")
 
