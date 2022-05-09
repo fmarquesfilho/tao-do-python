d64833a4b879433f251e4a27b5bf34a62b4f1657
Fernando Filho <fernando@dimap.ufrn.br>
2022-05-09 11:17:49 -0300

    exercício 3 atualizado

--- lista1/3-par-ou-impar-fazer.py
+++ lista1/3-par-ou-impar-fazer.py
@@ -1,5 +1,16 @@
 '''
 Escreva um programa que lê dois números inteiros, 
 e imprime uma mensagem caso apenas um dos números
-seja ímpar e o outro seja par.
+seja ímpar e o outro seja par. O programa não 
+deve dizer nada quando ambos foram pares ou
+quando ambos forem ímpares.
 ''' 
+# Recebe um número inteiro e retorna verdadeiro caso ele seja par e falso
+# quando ele for ímpar.
+def par(i):
+    r = i % 2
+    return r == 0
+
+def impar(i):
+    return not par(i)
+
